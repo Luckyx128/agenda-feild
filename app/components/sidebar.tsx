6253a5c52b00f29e43ebc810e7945162ee685a6a
Lucas <lucas.de.amorimarq@gmail.com>
2025-04-29 20:16:00 -0300

    feat: enhance sidebar layout and styling; adjust calendar container width

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -15,8 +15,27 @@ export default function Sidebar() {
 				<h1>Nome APP</h1>
 			</section>
 			<nav className="sidebar-nav">
+				<h2 className='sidebar-nav-label'>Agendas</h2>
+				<ul className="sidebar-nav-list">
+					<li className="sidebar-nav-item">
+						<button className="sidebar-nav-button"></button>
+					</li>
+					<li className="sidebar-nav-item">
+						<a href="/eventos">Eventos</a>
+					</li>
+					<li className="sidebar-nav-item">
+						<a href="/relatorios">Relatórios</a>
+					</li>
+					<li className="sidebar-nav-item">
+						<a href="/configuracoes">Configurações</a>
+					</li>
+				</ul>
 			</nav>
 			<section className="sidebar-footer">
+				<div className="sidebar-footer-content">
+					<p>Nome do usuário</p>
+					<button className="sidebar-footer-button">Sair</button>
+				</div>
 			</section>
 		</div>
 	);
